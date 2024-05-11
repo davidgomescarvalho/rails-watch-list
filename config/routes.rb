4e8a7f6e4783685b4a8d70fddbbb699192be8594
david gomes <davidgomesk8@gmail.com>
2024-05-11 16:50:23 +0200

    back-end done, front to do

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
